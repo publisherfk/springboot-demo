b1cf97a20cebcf3f7787dc42a3bda87c1981b335
傅昆 <fukun@supcon.com>
2020-10-11 11:06:07 +0800

    初次提交

---
+++ src/main/java/com/imoo/heshi/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.imoo.heshi.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
